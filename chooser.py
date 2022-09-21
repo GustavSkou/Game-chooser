f43f2929cb63d9081ab1cb731e22e0937ee28a56
GustavSkou <55804968+GustavSkou@users.noreply.github.com>
2022-09-21 23:49:14 +0200

    1.0
    Everything should be working as intended. So version 1.0 :-) :catjam:

--- chooser.py
+++ chooser.py
@@ -1,31 +1,70 @@
 import random
+random.seed()
+
+short = 1
+medium = 2
+long = 3
+forever = 4
+y = 1
+x = 1
 
 # Creating list of lists
     # each row's item[0] is being represented by 3 categories
-games = [["Escape from tarkov", "multiplayer", "pvpve", "long"],
-    ["League of legends", "multiplayer", "pvp", "medium"],
-    ["Rocket league", "multiplayer", "pvp", "short"],
-    ["Minecraft", "multiplayer", "pve", "long"],
-    ["Shatterline" "multiplayer", "pve", "medium"],
-    ["Among us", "multiplayer", "pvp", "short"],
-    ["Rainbow six siege", "multiplayer", "pvp", "medium"],
-    ["Phasmophobia", "multiplayer", "pve", "medium"],
-    ["Euro truck simulator 2", "multiplayer", "driving"]]
+games = [["Escape from tarkov", "multiplayer", "pvpve", long],
+    ["League of legends", "multiplayer", "pvp", short],
+    ["Rocket league", "multiplayer", "pvp", short],
+    ["Minecraft", "multiplayer", "pve", forever],
+    ["Shatterline" "multiplayer", "pve", medium],
+    ["Among us", "multiplayer", "pvp", short],
+    ["Rainbow six siege", "multiplayer", "pvp", medium],
+    ["Phasmophobia", "multiplayer", "pve", medium],
+    ["Euro truck simulator 2", "multiplayer", "driving", long]]
+
+game_types = ["pvp", "pve", "pvpve", "driving"]         # List of the type of games, item(2) in games
 
 # input type
-print("Hvilken type spil vil du spille")
-type = input()
+print("What kind of game would you like to play (pvp, pve, pvpve, driving)")
+while y == 1:
+    type = input()
+    if type in game_types:
+        y = 0
+    else:
+        print("try again")
 
 # input length
-print("Hvor lang tid vil du spille")
-length = input()
+print("For how long do you want to play (short, medium, long, forever)")
+while x == 1:
+    length = input()
+    if length == "short":
+        length = 1
+        x = 0
+    elif length == "medium":
+        length = 2
+        x = 0
+    elif length == "long":
+        length = 3
+        x = 0
+    elif length == "forever":
+        length = 4
+        x = 0
+    else:
+        print("try again")
 
-to_play = []                                            # create an emtpy list for the games to play
+to_play = []                                            # Create an emtpy list for the games to play
 for item in games:
     if item[2] == type and item[3] == length:
         to_play.append(item[0])                         # Add games to the to_play list
-to_play_size = len(to_play)                             # check how many corresponding games there is
+to_play_size = len(to_play)                             # Check how many corresponding games there is
 
 if to_play_size >= 2:                                   # If the list contains more than 1 game
-    ran = random.uniform(0, to_play_size-1)
-    print(to_play[int(ran)])
+    ran = random.uniform(0, to_play_size)               # Gets random number to the size of how many games were found
+    print(to_play[int(ran)])                            # Uses random number to print
+elif to_play_size == 0:                                 # If there wasn't any corresponding games
+    for item in games:
+        if item[2] == type and item[3] == length-1 or item[2] == type and item[3] == length+1:  # Check if theres is corresponding games to the type and the length +- 1
+            to_play.append(item[0])
+    to_play_size = len(to_play)
+    ran = random.uniform(0, to_play_size)               # Gets random number to the size of how many games were found
+    print(to_play[int(ran)])                            # Uses random number to print
+else:                                                   # If the list only contains 1 game
+    print(to_play)
